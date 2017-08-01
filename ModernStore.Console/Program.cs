6c05dbd21aeade889d3ee3cdf1145fc09ba99dde
stanleyalves <stanleyalves@live.com>
2017-08-01 13:21:51 -0300

    Inclusão de cadastro de Itens do Pedido

---
+++ ModernStore.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ModernStore.Domain.Entities;
+
+namespace ModernStore.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("Stanley",
+                "Alves",
+                new DateTime(1971, 8, 8),
+                "stanley",
+                "stanley.alv3s@gmail.com");
+            
+            customer.Activate();
+
+            System.Console.WriteLine($"{customer.ToString()}");
+            System.Console.ReadKey();
+        }
+    }
+
+}
---
+++ ModernStore.Domain/Entities/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ModernStore.Domain.Entities
+{
+    public class Customer
+    {
+        public Customer(string firstName, string lastName, DateTime? birthDate, string password, string email)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Active = false;
+            UserName = email;
+            Password = password;
+            Email = email;
+
+            //Validações
+
+        }
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public bool Active { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+
+
+        public void Activate()
+        {
+            Active = true;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
